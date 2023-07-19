dc048421e8016b5c6caa514371d569859cde2bbd
parkchagnsung <pcsgod200@naver.com>
2023-07-19 14:15:57 +0900

    BMW_JIRA_KEY_4 개발 4 - 파일 추가

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,8 @@ public class Main {
         System.out.println("Hello world!!");
         System.out.println("Hello world!!!");
     }
+    public static void test2(){
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!!");
+    }
 }
